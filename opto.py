8b6fddabb7f833337a1ced8977b3a13542667a8c
SeanGormann <seangorman117@gmail.com>
2024-05-24 10:52:01 +0000

    Working Parralleellalllelisms

--- opto.py
+++ opto.py
@@ -11,6 +11,8 @@ from tqdm import tqdm
 import torch.nn as nn
 from torch.optim.lr_scheduler import CosineAnnealingLR, ReduceLROnPlateau
 from torch.cuda.amp import GradScaler, autocast
+from utils import *
+
 
 class GraphDataset(Dataset):
     def __init__(self, flat_bbs, graph_dict, ys):
@@ -30,33 +32,58 @@ class GraphDataset(Dataset):
         
         return bb1, bb2, bb3, ys
 
+import numpy as np
+import pandas as pd
+from torch.utils.data import Dataset
+from sklearn.model_selection import KFold
+
+class GraphDataset(Dataset):
+    def __init__(self, flat_bbs, graph_dict, ys, fold=0, nfolds=5, train=True, seed=2023):
+        self.flat_bbs = flat_bbs
+        self.graph_dict = graph_dict
+        self.ys = ys
+        
+        # K-Fold Splitting
+        kf = KFold(n_splits=nfolds, shuffle=True, random_state=seed)
+        indices = np.arange(len(self.flat_bbs) // 3)
+        folds = list(kf.split(indices))
+        train_idx, val_idx = folds[fold]
+        
+        if train:
+            selected_idx = train_idx
+        else:
+            selected_idx = val_idx
+
+        selected_flat_bbs = []
+        selected_ys = []
+        for idx in selected_idx:
+            start_idx = idx * 3
+            selected_flat_bbs.extend(self.flat_bbs[start_idx:start_idx + 3])
+            selected_ys.append(self.ys[idx])
+
+        self.flat_bbs = np.array(selected_flat_bbs)
+        self.ys = np.array(selected_ys)
+
+    def __len__(self):
+        return len(self.flat_bbs) // 3
+
+    def __getitem__(self, idx):
+        start_idx = idx * 3
+        bb1 = self.graph_dict[self.flat_bbs[start_idx]]
+        bb2 = self.graph_dict[self.flat_bbs[start_idx + 1]]
+        bb3 = self.graph_dict[self.flat_bbs[start_idx + 2]]
+        ys = self.ys[idx]
+        
+        return bb1, bb2, bb3, ys
+
+
+
 def custom_collate_fn(batch):
     graphs_array_1 = [item[0] for item in batch]
     graphs_array_2 = [item[1] for item in batch]
     graphs_array_3 = [item[2] for item in batch]
     ys_array = np.array([item[3] for item in batch])  # Convert list of numpy arrays to a single numpy array
     return Batch.from_data_list(graphs_array_1), Batch.from_data_list(graphs_array_2), Batch.from_data_list(graphs_array_3), torch.tensor(ys_array)
-
-# Load your dataset
-loaded_data = np.load('data/dataset.npz')
-loaded_buildingblock_ids = loaded_data['buildingblock_ids']
-loaded_targets = loaded_data['targets']
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-print(f'Loaded buildingblock_ids shape: {loaded_buildingblock_ids.shape}')
-
-len_dataset = len(loaded_buildingblock_ids)
-batch_size = 2048
-
-graphs = pd.read_pickle('data/all_buildingblock.pkl')
-graph_dict = {row['id']: row['mol_graph'] for _, row in graphs.iterrows()}
-
-# Flatten the loaded_buildingblock_ids for batching
-flat_bbs = loaded_buildingblock_ids.flatten()
-
-# Create dataset and dataloader
-dataset = GraphDataset(flat_bbs, graph_dict, loaded_targets)
-dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=6, collate_fn=custom_collate_fn, pin_memory=True)
 
 
 class MolGNN2(torch.nn.Module):
@@ -123,8 +150,32 @@ class MolGNN2(torch.nn.Module):
         x = global_mean_pool(x, batch)
         return x
 
-criterion = nn.BCEWithLogitsLoss()  #pos_weight=weights[1]
-scaler = GradScaler()
+
+def process_batch(batch, train=True, protein_index=None):
+        with autocast():
+            outputs = model(batch)
+    
+            # Adjust reshaping based on your batch data structure and need
+            if protein_index is not None:
+                y_vals = batch[3].view(-1, 3)[:, protein_index].view(-1, 1)  # Select the correct targets and reshape
+            else:
+                y_vals = batch[3].view(-1, 3)  #
+    
+            y_vals = y_vals.float().to(device)
+    
+            #loss = criterion(outputs.squeeze(), y_vals)
+            loss = criterion(outputs, y_vals)
+    
+        if train:
+            optimizer.zero_grad()
+            scaler.scale(loss).backward()
+            scaler.step(optimizer)
+            scaler.update()
+    
+        return outputs.detach(), y_vals.detach(), loss.item()
+
+import torch
+from torch.cuda.amp import autocast, GradScaler
 
 def process_batch(batch, train=True, protein_index=None):
     with autocast():
@@ -134,11 +185,14 @@ def process_batch(batch, train=True, protein_index=None):
         if protein_index is not None:
             y_vals = batch[3].view(-1, 3)[:, protein_index].view(-1, 1)  # Select the correct targets and reshape
         else:
-            y_vals = batch[3].view(-1, 3)  #
+            y_vals = batch[3].view(-1, 3)
 
-        y_vals = y_vals.float().to(device)
+        y_vals = y_vals.float().to(device)  # Use the device from the batch data
 
-        #loss = criterion(outputs.squeeze(), y_vals)
+        # Ensure the target size matches the input size
+        if outputs.size() != y_vals.size():
+            raise ValueError(f"Target size ({y_vals.size()}) must be the same as input size ({outputs.size()})")
+
         loss = criterion(outputs, y_vals)
 
     if train:
@@ -150,8 +204,11 @@ def process_batch(batch, train=True, protein_index=None):
     return outputs.detach(), y_vals.detach(), loss.item()
 
 
-def train_model(model, dataloader, num_epochs, targets):
+def train_model(model, dataloader, test_dataloader, num_epochs, targets, scaler, optimizer, criterion):
     #model = model.cuda()  # Move the model to GPU
+    #model = torch.nn.DataParallel(model) 
+    model = torch.nn.DataParallel(model) 
+    model = model.cuda()  
 
     for epoch in range(num_epochs):
         model.train()
@@ -161,37 +218,87 @@ def train_model(model, dataloader, num_epochs, targets):
         with tqdm(total=len(dataloader), desc=f"Epoch {epoch+1}") as pbar:
             for data in dataloader:
                 #print(data)
-                outs, labs, loss = process_batch(data, train=True, protein_index=None)
+                outs, labs, loss = process_batch(data, train=True, protein_index=None) # (data, model, scaler, optimizer, criterion, train=True, protein_index=None)
                 total_loss += loss
-                epoch_loss += 1
-                if epoch_loss % 100 == 0:
-                    print(loss)
-        
+                
                 train_outputs.append(outs.cpu().numpy())  # Collect outputs for MAP calculation
                 train_targets.append(labs.cpu().numpy())
                 
                 pbar.set_postfix(loss=epoch_loss / (pbar.n + 1))
                 pbar.update()
+
+                if pbar.n % 5000 == 0 and pbar.n != 0:
+                    map_brd4, map_hsa, map_seh, average_map, true_positives_brd4, predicted_positives_brd4, \
+                    true_positives_hsa, predicted_positives_hsa, true_positives_seh, predicted_positives_seh = calculate_individual_map(train_targets, train_outputs)
+                    print(f"Average MAP: {average_map}, BRD4, HSA, sEH MAP: {map_brd4}, {map_hsa}, {map_seh} \nPositive Predicted/Truths: \
+                    {predicted_positives_brd4}/{true_positives_brd4}, {predicted_positives_hsa}/{true_positives_hsa}, \
+                    {predicted_positives_seh}/{true_positives_seh}")
                 
         print(f"Epoch {epoch+1} Loss: {epoch_loss / len(dataloader)}")
+        
+        val_outputs, val_targets = [], []
+
+        model.eval()
+        with tqdm(total=len(test_dataloader), desc=f"Epoch {epoch+1}") as pbar:
+            for data in dataloader:
+                #print(data)
+                outs, labs, loss = process_batch(data, train=True, protein_index=None) 
+                total_loss += loss
+
+                val_outputs.append(outs.cpu().numpy())  # Collect outputs for MAP calculation
+                val_targets.append(labs.cpu().numpy())
+                pbar.set_postfix(loss=epoch_loss / (pbar.n + 1))
+                pbar.update()
+                    
+                map_brd4, map_hsa, map_seh, average_map, true_positives_brd4, predicted_positives_brd4, true_positives_hsa, \
+                predicted_positives_hsa, true_positives_seh, predicted_positives_seh = calculate_individual_map(train_targets, train_outputs)
+                print(f"Val Average MAP: {average_map}, BRD4, HSA, sEH MAP: {map_brd4}, {map_hsa}, {map_seh} \nPositive Predicted/Truths: \
+                    {predicted_positives_brd4}/{true_positives_brd4}, {predicted_positives_hsa}/{true_positives_hsa}, \
+                    {predicted_positives_seh}/{true_positives_seh}")
+
 
 if __name__ == '__main__':
     mp.set_start_method('spawn')
 
+    #Hyperparams
     num_epochs = 10
     initial_lr = 0.0001
     do = 0.1
-    
-    mol_node_features = 8 # 8 11
-    mol_edge_features = 4
+    batch_size = 2048
 
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    mol_node_features = 8 # 8 11
     model = MolGNN2(mol_node_features, 4, 96, bb_dims=(320, 320, 320), device=device)
     model.to(device)
-    print(model)
-    print(f'Number of parameters: {sum(p.numel() for p in model.parameters())}')
+    print(f'Model: {model} \nNumber of parameters: {sum(p.numel() for p in model.parameters())}')
     
+    criterion = nn.BCEWithLogitsLoss()  #pos_weight=weights[1]
+    scaler = GradScaler()
+    optimizer = torch.optim.Adam(model.parameters(), lr=initial_lr) 
     
-    optimizer = torch.optim.Adam(model.parameters(), lr=initial_lr) #lr=0.00
 
+    #Get Data
+    # Load your dataset
+    loaded_data = np.load('data/dataset.npz')
+    loaded_buildingblock_ids, loaded_targets = loaded_data['buildingblock_ids'], loaded_data['targets']
+    print(f'Loaded buildingblock_ids shape: {loaded_buildingblock_ids.shape}')
+    len_dataset = len(loaded_buildingblock_ids)
+    
+    
+    graphs = pd.read_pickle('data/all_buildingblock.pkl')
+    graph_dict = {row['id']: row['mol_graph'] for _, row in graphs.iterrows()}
+    
+    # Flatten the loaded_buildingblock_ids for batching
+    flat_bbs = loaded_buildingblock_ids.flatten()
+
+    print("Prepping Dataloaders - eta 6mins")
+    # Create dataset and dataloader
+    dataset = GraphDataset(flat_bbs, graph_dict, loaded_targets, fold=0, nfolds=5, train=True, seed=2023)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=10, collate_fn=custom_collate_fn, pin_memory=True)
+    
+    test_dataset = GraphDataset(flat_bbs, graph_dict, loaded_targets, fold=0, nfolds=5, train=False, seed=2023)
+    test_dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=4, collate_fn=custom_collate_fn, pin_memory=True)
+    
     # Train the model
-    train_model(model, dataloader, num_epochs, loaded_targets)
+    print("Starting training")
+    train_model(model, dataloader, test_dataloader, num_epochs, loaded_targets, scaler, optimizer, criterion)
---
+++ utils.py
@@ -0,0 +1,93 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.cuda.amp import GradScaler, autocast
+from sklearn.metrics import average_precision_score
+import numpy as np
+
+
+
+def update_learning_rate(optimizer, epoch, batch_idx, total_steps, warmup_steps, initial_lr):
+    """Update learning rate based on warmup and decay."""
+    if epoch * len(train_dataset) + batch_idx < warmup_steps:
+        # Warm-up phase: linear increase
+        lr = initial_lr * (epoch * len(train_dataset) + batch_idx + 1) / warmup_steps
+    else:
+        # Exponential decay
+        decay_rate = 0.95  # Decay rate per epoch after warmup
+        lr = initial_lr * (decay_rate ** (epoch - warmup_steps / len(train_dataset)))
+
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+
+
+class FocalLoss(nn.Module):
+    def __init__(self, alpha=0.25, gamma=2.0, reduction='mean'):
+        super(FocalLoss, self).__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+        self.reduction = reduction
+
+    def forward(self, inputs, targets):
+        BCE_loss = F.binary_cross_entropy_with_logits(inputs, targets, reduction='none')
+        pt = torch.exp(-BCE_loss)  # Prevents nans when probability 0
+        F_loss = self.alpha * (1-pt)**self.gamma * BCE_loss
+
+        if self.reduction == 'mean':
+            return torch.mean(F_loss)
+        elif self.reduction == 'sum':
+            return torch.sum(F_loss)
+        else:
+            return F_loss
+
+
+from sklearn.metrics import average_precision_score
+import numpy as np
+
+def calculate_individual_map(y_true, y_scores):
+    """
+    Calculate the mean average precision (MAP) for each protein individually and return their average.
+
+    Args:
+    y_true (np.array): True labels reshaped to (-1, 3) where each column is a protein.
+    y_scores (np.array): Predicted scores reshaped to (-1, 3) where each column is a protein.
+
+    Returns:
+    tuple: A tuple containing individual MAPs for BRD4, HSA, sEH, and the average MAP.
+    """
+    # Ensure y_true and y_scores are reshaped correctly
+    y_true = np.array(y_true, dtype=np.float32).reshape(-1, 3)
+    y_scores = np.array(y_scores, dtype=np.float32).reshape(-1, 3)
+
+    # Print statements for debugging
+    #print(f"y_true shape: {y_true.shape}, dtype: {y_true.dtype}")
+    #print(f"y_scores shape: {y_scores.shape}, dtype: {y_scores.dtype}")
+
+    # Convert continuous predictions to binary labels
+    y_pred_binarized = (y_scores > 0.5).astype(int)
+
+    #print(f"y_pred_binarized shape: {y_pred_binarized.shape}, dtype: {y_pred_binarized.dtype}")
+    #print(f"y_true: {y_true[:, 0]}, scores { y_scores[:, 0]} ")
+
+    # Calculate MAP for each column (protein)
+    map_brd4 = average_precision_score(y_true[:, 0], y_scores[:, 0])
+    map_hsa = average_precision_score(y_true[:, 1], y_scores[:, 1])
+    map_seh = average_precision_score(y_true[:, 2], y_scores[:, 2])
+
+    # Calculate the average MAP across all proteins
+    average_map = np.mean([map_brd4, map_hsa, map_seh])
+
+    # Calculate true positives and predicted positives
+    threshold = 0.5
+    true_positives_brd4 = np.sum((y_true[:, 0] == 1) & (y_scores[:, 0] > threshold))
+    predicted_positives_brd4 = np.sum(y_scores[:, 0] > threshold)
+    
+    true_positives_hsa = np.sum((y_true[:, 1] == 1) & (y_scores[:, 1] > threshold))
+    predicted_positives_hsa = np.sum(y_scores[:, 1] > threshold)
+    
+    true_positives_seh = np.sum((y_true[:, 2] == 1) & (y_scores[:, 2] > threshold))
+    predicted_positives_seh = np.sum(y_scores[:, 2] > threshold)
+
+    return map_brd4, map_hsa, map_seh, average_map, true_positives_brd4, predicted_positives_brd4, true_positives_hsa, predicted_positives_hsa, true_positives_seh, predicted_positives_seh
+
